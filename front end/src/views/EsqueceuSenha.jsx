1a307e7270e1380a8b37b962312fe7772a30c2f7
Mersun <ecomercialmersun@gmail.com>
2024-04-15 22:27:24 -0300

    paginaSobre

--- front end/src/views/EsqueceuSenha.jsx
+++ front end/src/views/EsqueceuSenha.jsx
@@ -5,7 +5,7 @@ import EsqueceuSenhaCom from '../components/EsqueceuSenhaCom';
 const EsqueceuSenha = () => {
  return (
     <div className="columns-2 h-screen">
-    <div className="bg-gradient-to-t from-blue-950 to-azulClaro h-screen">
+    <div className="bg-azul h-screen">
       <img className=" pl-80 pt-80 " src={logo}></img>
     </div>
     <div className="h-full place-content-center">
