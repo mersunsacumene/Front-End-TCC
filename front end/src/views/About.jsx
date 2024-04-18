1fdeff361c3b1f6b15ecc62cdc8f11da66c75ac4
Mersun <ecomercialmersun@gmail.com>
2024-04-17 22:37:57 -0300

    paginaSobre

--- front end/src/views/About.jsx
+++ front end/src/views/About.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
-import about from "../assets/about.jpg"
+import sobre from "../assets/sobre.jpg"
 const About = () => {
     return (
        <div>
-        <div className=''> 
-            <img className='w-full h-96 fixed opacity-50'src={about}/>
-            <h1 className='text-center'>Sobre o Dazzle</h1>
-            <h3 className='text-center'>Somos um grupo criado na fiec e desenvolvemos uma ferramenta chamada For M.E.I para ajudar os prestadores de serviços e aqueles que precisam dos serviços</h3>
-        </div>
+        <div className='static'> 
+            <img className='w-full h-1/2 fixed opacity-50'src={sobre}/>
+            </div>
+
+            <div className='flex flex-col text-center fixed top-48 '>
+                <div>Sobre o Dazzle</div>
+                <div>Somos um grupo criado na fiec e desenvolvemos uma ferramenta chamada For M.E.I para ajudar os prestadores de serviços e aqueles que precisam dos serviços</div>
+            </div>
         
-            <div>
+            <div className='flex flex-col-3 fixed justify-center top-2/3 ml-40 mr-40 space-x-16'>
+
+                <div><p>Quem somos</p>
+                    <p className='text-lg text-justify text-wrap'>Somos um grupo de estudantes da FIEC que juntos criamos uma plataforma para ajudar o trabalhador que precisa de divulgação e também o cliente que precisa de serviços</p>
+                </div>
+                <div>O que fazemos?</div>
+                <div>Quer se associar</div>
+
             </div>
         
 
             <div>
+
             </div>
        </div>
     )
--- front end/src/views/Contact.jsx
+++ front end/src/views/Contact.jsx
@@ -6,57 +6,38 @@ const Contact = () => {
     return (
        
         <div class="flex flex-row ">
-       <div className="bg-white w-1/3 h-lvh">
-      <img className=" pl-48 pt-96" src={logo2}></img>
+       <div className=" w-full h-lvh text-center mt-96">
+        <p>Olá, somos o grupo DAZZLE, contate-nos!</p>
     </div>
-        <div class="bg-white w-1/3 max-w-4xl p-16">
-          <p className='text-azul text-center'><h1>Contato</h1></p>
-          <form class="bg-azul mt-40 rounded-xl w-full max-w-4xl p-8 shadow-lg space-y-6" action="#" method="POST">
+        <div class="bg-white w-1/2 max-w-4xl p-16">
+          <form class=" border-4 border-azul mt-40 rounded-xl w-full max-w-4xl p-8 shadow-lg space-y-6" action="#" method="POST">
           <div>
               <label for="name" class="block text-sm font-medium leading-6 text-white">Nome completo </label>
               <div class="mt-2">
-                <input id="name" name="name"  type='text' placeholder="Nome completo" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-            <div>
-              <label for="date" class="block text-sm font-medium leading-6 text-white">Data de nascimento </label>
-              <div class="mt-2">
-                <input id="name" name="date" type='date' required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-            <div>
-              <label for="telefone" class="block text-sm font-medium leading-6 text-white">Telefone</label>
-              <div class="mt-2">
-                <input id="telefone" name="telefone" type="tel" pattern="[0-9]{2} [0-9]{5}-[0-9]{4}" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6"  placeholder="xx xxxxx-xxxx" />
+                <input id="name" name="name"  type='text' placeholder="Nome completo" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-azul placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6" />
               </div>
             </div>
             <div>
               <label for="email" class="block text-sm font-medium leading-6 text-white">Email </label>
               <div class="mt-2">
-                <input id="email" name="email" type="email" placeholder="Email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6" />
+                <input id="email" name="email" type="email" placeholder="Email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-azul placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6" />
               </div>
             </div>
             <div>
-            </div>
-            <div>
-              <label for="name" class="block text-sm font-medium leading-6 text-white">Assunto</label>
-              <div class="mt-2">
-                <input id="text" type='text' placeholder="Nome completo" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6" />
-              </div>
             </div>
             <div>
               <label for="name" class="block text-sm font-medium leading-6 text-white">Mensagem </label>
               <div class="mt-2">
-                <input id="text" type='text' placeholder="Nome completo" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6" />
+                <input id="text" type='text' placeholder="Digite sua mensagem" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-azul placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-azul sm:text-sm sm:leading-6" />
               </div>
             </div>
             <div>
-             <button type="submit" class="flex w-full justify-center rounded-md bg-white px-3 py-1.5 text-sm font-semibold leading-6 text-azul shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar Mensagem</button>
+             <button type="submit" class="flex w-full justify-center rounded-md bg-azul text-white px-3 py-1.5 text-sm font-semibold leading-6 text-azul shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar Mensagem</button>
             </div>
           </form>
         </div>
         <div className="bg-white w-1/3 h-lvh">
-      <img className=" pl-48 pt-96 " src={logo2}></img>
+      
     </div>
    
         <Footer />
